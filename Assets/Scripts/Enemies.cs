915132a2243885c5b711236824d0f0226ad7c3aa
Andres <andres.leay.e@gmail.com>
2022-07-27 15:46:21 -0400

    Add: ENCAPSULATION en enemigos(speed)

--- Assets/Scripts/Enemies.cs
+++ Assets/Scripts/Enemies.cs
@@ -2,21 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-/* public class Enemies : MonoBehaviour
-{
-
-} */
-
-
 
 public class Enemies : MonoBehaviour
 {
-[SerializeField] protected float speed = 12.0f;
+[SerializeField] private int _speed;
+Enemy enemy = new Enemy(); // ENCAPSULATION
+
 private SpriteRenderer _renderer;
 private int dir = 1;
 
 protected virtual void Start() {
     _renderer = GetComponent<SpriteRenderer>();
+    enemy.RunSpeed = _speed;
 }
 
 protected virtual void Update() {
@@ -24,14 +21,28 @@ protected virtual void Update() {
 }
 
 private void Move(int dir){
-    transform.Translate(Vector2.left * speed * Time.deltaTime * dir);
+    transform.Translate(Vector2.left * enemy.RunSpeed * Time.deltaTime * dir);
 }
 
-private void OnTriggerEnter2D(Collider2D other) {
+protected virtual void OnTriggerEnter2D(Collider2D other) {
     if(other.gameObject.CompareTag("LimitPlatform")){
         dir *= -1;
         _renderer.flipX = !_renderer.flipX;
     }
 }
 
+}
+
+// ENCAPSULATION
+public class Enemy { 
+    private int _runSpeed;
+    public int RunSpeed{
+    get{ return _runSpeed; }
+    set{ 
+        _runSpeed = value;
+        if(_runSpeed < 1){
+            _runSpeed = Random.Range(0,8);;
+        }
+        }
+    }
 }
--- Assets/Scripts/FlyEnemy.cs
+++ Assets/Scripts/FlyEnemy.cs
@@ -4,5 +4,8 @@ using UnityEngine;
 
 public class FlyEnemy : Enemies //INHERITANCE
 {
-    
+    override protected void OnTriggerEnter2D(Collider2D other) { //POLYMORPHISM
+            int posY = Random.Range(0,8);
+            gameObject.transform.position= new Vector3(gameObject.transform.position.x, posY, gameObject.transform.position.z);
+    }
 }
