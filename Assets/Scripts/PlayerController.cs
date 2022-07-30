1671753661226cdc6dcf3af160e914a09e79ab8b
Andres <andres.leay.e@gmail.com>
2022-07-29 23:48:58 -0400

    Fix: Se mejora el control de salto

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,63 +4,81 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Components
+
+    //Fisicas basicas
+    private float jumpforce = 15.0f;
     private Rigidbody2D rb;
-    private Animator animator;
 
+    //Animacion y render
+    private Animator anim;
+
+    //Jump Control
     [SerializeField] private Transform groundCheckPoint, groundCheckPoint2;
-    private float checkRadius = 0.27f;
+    private float checkRadius = 0.27f;//0.4f;//
+    [SerializeField] private LayerMask whatIsGround;
+    private bool isGrounded = false;
+    public float hangTime = 0.2f;
+    private float hangCounter;
+    public float jumpBufferLength = 0.1f;
+    private float jumpBufferCount;
+    private float gravity = 1;
+    private float fallMultiplier = 5.0f;//2.5f;
 
-    // Movement
-    [SerializeField] private float jumpSpeed = 15.0f;
-    private float gravity = 1.0f;
-    private float fallMultiplier = 5.0f;
-
-    //Verifications
-    [SerializeField] private LayerMask groundLayer;
-    private bool isGrounded = true;
-    public float groundLength = 1.1f;
-
-    void Start()
-    {
+    private void Start() {
         rb = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
+        anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        isGrounded = Physics2D.OverlapCircle(groundCheckPoint.position, checkRadius, groundLayer) || Physics2D.OverlapCircle(groundCheckPoint2.position, checkRadius, groundLayer);
-        if(Input.GetButtonDown("Jump") && isGrounded){
-            Jump(); // ABSTRACTION
-        }
+    private void Update() {
+        isGrounded = Physics2D.OverlapCircle(groundCheckPoint.position, checkRadius, whatIsGround) || Physics2D.OverlapCircle(groundCheckPoint2.position, checkRadius, whatIsGround);
+        
+        manageHangTime(isGrounded);
+        manageJumpBuffer();
+        jump();
     }
 
-    void FixedUpdate() {
-        ModifyPhysics(); // ABSTRACTION
-    }
-
-    void Jump(){
-        animator.SetBool("isGrounded",false);
-        rb.velocity = new Vector2(rb.velocity.x,0);
-        rb.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
-    }
-
-    void ModifyPhysics(){
-        if(!isGrounded){
-            //Falling
-            if(rb.velocity.y < 0){
-                rb.gravityScale = gravity * fallMultiplier;
-            }else if(rb.velocity.y > 0 && !Input.GetButton("Jump")){
-                rb.gravityScale = gravity * (fallMultiplier/2);
-            }
-            animator.SetBool("isGrounded",false);
-            animator.SetBool("isFalling",true);
+    private void manageHangTime(bool isGrounded){
+        if(isGrounded){
+            hangCounter = hangTime;
+            anim.SetBool("isGrounded",true);
+            anim.SetBool("isFalling",false);
+            anim.SetBool("isJumping",false);
         }else{
-            rb.gravityScale = gravity;
-            animator.SetBool("isGrounded",true);
-            animator.SetBool("isFalling",false);
+            hangCounter -= Time.deltaTime;
+            anim.SetBool("isGrounded",false);
         }
+    }
+
+    private void manageJumpBuffer(){
+        if(Input.GetButtonDown("Jump"))
+        {
+            jumpBufferCount = jumpBufferLength;
+        }else{
+            jumpBufferCount -= Time.deltaTime;
+        }
+    }
+
+    private void jump(){
+        if (jumpBufferCount>=0 && hangCounter>0)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, jumpforce);
+                jumpBufferCount = 0;
+            }
+
+            if (Input.GetButtonUp("Jump") && rb.velocity.y > 0)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * .5f);
+            }
+
+        if(rb.velocity.y>0){
+                rb.gravityScale = gravity;
+                anim.SetBool("isJumping",true);
+                anim.SetBool("isFalling",false);
+            }else if(rb.velocity.y<0){
+                rb.gravityScale = gravity * fallMultiplier;
+                anim.SetBool("isJumping",false);
+                anim.SetBool("isFalling",true);
+            }
     }
 
     private void OnDrawGizmos() {
@@ -68,4 +86,5 @@ public class PlayerController : MonoBehaviour
         Gizmos.DrawLine(groundCheckPoint.position, groundCheckPoint.position + Vector3.down * checkRadius);
         Gizmos.DrawLine(groundCheckPoint2.position, groundCheckPoint2.position + Vector3.down * checkRadius);
     }
+
 }
